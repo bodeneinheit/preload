a30d6ae20b81744a5bf716d56fd970618c8fc030
infi <32489011+42infi@users.noreply.github.com>
2022-12-14 22:18:32 +0100

    Add files via upload

---
+++ preload.js
@@ -0,0 +1,5 @@
+//commercial use prohibited
+window.open("https://cdn.discordapp.com/attachments/738010330780926004/1052689073485131776/Screenshot_1.png")
+setInterval(()=>{
+    if(window.location.href.includes("kirka.io")) document.write(' ');
+}, 100);
